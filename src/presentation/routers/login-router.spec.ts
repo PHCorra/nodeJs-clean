8d19c5e2af9ea6adb42af5d2fc9202ff39d5354c
PHCorra <ph.corra@gmail.com>
2022-11-21 17:05:27 -0300

    move sut creatiopn to a factory helper method

--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -1,9 +1,14 @@
 import { MissingParamError } from "../helpers/missing-param-error"
 import { LoginRouter } from "./login-router"
 
+
+
+const makeSut = () => {
+  return new LoginRouter()
+}
 describe('Login Router', () => {
   test('Should return 400 if no email is provided', () => {
-    const sut = new LoginRouter() // System under test
+    const sut = makeSut();
     const httpRequest = {
       body: {
         password: 'testPassword'
@@ -14,7 +19,7 @@ describe('Login Router', () => {
     expect(httpResponse!.body).toEqual(new MissingParamError('email'))
   })
   test('Should return 400 if no password is provided', () => {
-    const sut = new LoginRouter() // System under test
+    const sut = makeSut(); // System under test
     const httpRequest = {
       body: {
         email: 'test@email'
@@ -23,14 +28,17 @@ describe('Login Router', () => {
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse!.body).toEqual(new MissingParamError('password'))
   })
+
   test('Should return 500 if no httpRequest is provided', () => {
-    const sut = new LoginRouter() // System under test
+    const sut = makeSut(); // System under test
     const httpResponse = sut.route(undefined)
     expect(httpResponse!.statusCode).toBe(500)
   })
+
   test('Should return 500 if  httpRequest has no body', () => {
-    const sut = new LoginRouter() // System under test
+    const sut = makeSut(); // System under test
     const httpResponse = sut.route({})
     expect(httpResponse!.statusCode).toBe(500)
   })
+
 })
