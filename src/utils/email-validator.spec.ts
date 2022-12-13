96f6fe0b57c4d2fa3e2d164a8e16a5e1b6f98478
PHCorra <ph.corra@gmail.com>
2022-12-13 10:06:56 -0300

    code: ensure emailValidator is returning true

---
+++ src/utils/email-validator.spec.ts
@@ -0,0 +1,13 @@
+class EmailValidator {
+  isValid(email: string) {
+    return true
+  }
+}
+
+describe('Email Validator', () => {
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('valid_email@email.com')
+    expect(isEmailValid).toBe(true)
+  })
+})
